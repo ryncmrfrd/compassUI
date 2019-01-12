55afdeb36b8d9729646a8328c48767a7618e5760
Ryan Comerford <40415962+ryncmrfrd@users.noreply.github.com>
2019-01-13 08:58:18 +1100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var d = new Date();
 var months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
-var days = ["0","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"];
+var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 
 document.getElementById("date").innerHTML = " " + days[d.getDay()] + ", " +  d.getDate() + " " + months[d.getMonth()] + " " + d.getFullYear();s
